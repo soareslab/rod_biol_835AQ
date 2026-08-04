50e9e6171c78c79e5b8aca5fe0e434bdbad4fa80
Rodrigo Martin de Oliveira <hodorigo@hotmail.com>
2026-08-04 16:12:57 -0600

    commit

--- 01_script/FO.R
+++ 01_script/FO.R
@@ -33,10 +33,51 @@
 #### Load necessary libraries ####
 library(tidyverse)
 
-#### Read the RAW data from the .CSV file ####
-diet <- read.csv(file = "02_rawdata/raw_data.csv", header = TRUE, sep = ",")
-grouping <- read.csv(file = "02_rawdata/grouping.csv", header = TRUE, sep = ",")
-
+#### Read the RAW data from the .CSV file, right now I add a function to auto_gen a fish data-set, the goal is to write a script that is capable of receiving different data-seets, and still perfomr the task of FO and Iai ####
+function(auto_gen_fish_data) {
+  set.seed(123) # for reproducibility
+  
+  # Number of fish sampled
+  n_fish <- 800
+  
+  # Four sampling areas
+  sampling_areas <- c("Area_1", "Area_2", "Area_3", "Area_4")
+  
+  # Example fish species names
+  species_list <- c(
+    "Astyanax_fasciatus",
+    "Hoplias_malabaricus",
+    "Leporinus_friderici",
+    "Prochilodus_lineatus",
+    "Pimelodus_maculatus",
+    "Cichla_ocellaris",
+    "Geophagus_brasiliensis",
+    "Rhamdia_quelen"
+  )
+  
+  # Number of prey categories (you can change this)
+  n_prey <- 60
+  
+  # Create main columns
+  fish_data <- data.frame(
+    Sampling_area = sample(sampling_areas, n_fish, replace = TRUE),
+    Fish_uniq_id = sprintf("F%04d", 1:n_fish),
+    Species = sample(species_list, n_fish, replace = TRUE),
+    stringsAsFactors = FALSE
+  )
+  
+  # Add prey-volume columns with values between 0.01 and 1.00
+  for (i in 1:n_prey) {
+    fish_data[[paste0("prey_", i)]] <- round(runif(n_fish, min = 0.01, max = 1.00), 2)
+  }
+  
+  # Add some zeros to simulate absence of prey items (35% chance of being zero)
+  fish_data[[paste0("prey_", i)]] <- ifelse(
+    runif(n_fish) < 0.35,
+    0,
+    round(runif(n_fish, min = 0.01, max = 1.00), 2)
+  )
+}
 
 ##### Find and replace PI for puddle1, PII for puddle2, PV for puddle3 and Perene for flowing river in the column "sampling_area" ####
 diet <- diet %>%
