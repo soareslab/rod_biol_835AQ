61a154f1c88a7a36338e59ee22547064469be489
Rodrigo Martin de Oliveira <hodorigo@hotmail.com>
2026-08-10 15:59:18 -0600

    commit

--- 01_script/FO.R
+++ 01_script/FO.R
@@ -1,22 +1,22 @@
-## Author: Rodrigo Martin de Oliveira
+#### Author: Rodrigo Martin de Oliveira ####
 
 #### Load necessary libraries ####
 library(tidyverse)
 library(janitor)
 
 source("01_script/functions.R")
-#### Read the RAW data from the .CSV file
+#### Read the RAW data from the .CSV file ####
 
 fish_data <- read.csv("02_rawdata/generated_fish_data.csv", header = TRUE, stringsAsFactors = FALSE)
 
-#### cleaning and validation step 
+#### cleaning and validation step #####
 
 fish_data <- clean_diet_data(fish_data)
 
-#### here you can select the variables you want to use in the analysis 
+#### here you can select the variables you want to use in the analysis #####
 
 species <- fo_summary(fish_data, group_vars = "species")
 sampling_area <- fo_summary(fish_data, group_vars = "sampling_area")
 sp_area <- fo_summary(fish_data, group_vars = c("species", "sampling_area"))
 
-# END OF THE SCRIPT
+#### END OF THE SCRIPT #####
